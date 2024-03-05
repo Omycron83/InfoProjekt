8ee366e87f1d7c1b72be582b412aac86f2752796
Damian <damian.grunert@gmx.de>
2024-03-05 23:31:55 +0100

    Initial work with flask, Chiaras stuff

--- WebServer.py
+++ WebServer.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        button_pressed = request.form.get('button_pressed', False)
+        return render_template('result.html', button_pressed=button_pressed)
+
+
+if __name__ == "__main__":
+    app.run()
