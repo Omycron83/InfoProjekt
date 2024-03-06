5cc64bb06e7419d7342dbcaaa6f92c5033fc1a87
Damian Grunert <damian.grunert@gmx.de>
2024-03-06 11:28:41 +0100

    finished xgboost

--- XGBoost.py
+++ XGBoost.py
@@ -1,10 +1,26 @@
 import hyperparmeter_optimization
 from skopt.space import Real, Integer
 import xgboost
+import numpy as np
+
+"""
+Parameters: params[0] = gamma, params[1] = learning_rate, params[2] = max_depth, params[3] = n_estimators, params[4] = sub_sample, params[5] = min_child_weight, params[6] = reg_alpha, params[7] = reg_lambda
+
+XGBoostRegr: Base-class for facilitating the model itself
+XGBoostAutoRegr: Class providing a higher-level mask that facilitates the model search and the prediction
+"""
+
+def MSE(pred, Y):
+    return np.sum((np.array(pred) - np.array(Y).reshape(np.array(pred).shape))**2) / (2 * np.array(pred).size)
 
 class XGBoostRegr:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, params) -> None:
+        self.xgboost_reg = xgboost.XGBRegressor(gamma = params[0], learning_rate = params[1], max_depth = params[2], n_estimators = params[3], n_jobs = 16, objective = 'reg:squarederror', subsample = params[4], scale_pos_weight = 0, reg_alpha = params[6], reg_lambda = params[7], min_child_weight = params[5])
+
+    def train(self, features, labels):
+        self.xgboost_reg.fit(features, labels)
+    def predict(self, features):
+        self.xgboost_reg.predict(features)
 
 class XGBoostAutoRegr:
     def __init__(self, features, labels) -> None:
@@ -12,13 +28,17 @@ class XGBoostAutoRegr:
         self.train_labels = labels
         self.opt_hyperparams = self.optimize_xgboost(features, labels)
         self.model = XGBoostRegr(self.opt_hyperparams)
-
-
-    def train_xboost(self, features, labels):
-        pass
+        self.optimize_xgboost(features, labels)
+        
+    def train_xgboost(self, features, labels):
+        self.model.train(features, labels)
 
     def optimize_xgboost(self, features, labels):
         self.opt_hyperparams = hyperparmeter_optimization.find_opt_hyperparameters([Integer(1, 8192), Real(0, 0.9999), Integer(1, 128), Real(0.0001, 0.1), Real(0, 20)], xgboost.XGBRegressor, xgboost.MSE)
-    
+        self.train_xgboost(features, labels)
+
     def pred(self, new_features):
         return self.model.predict(new_features)
+    
+    def cost(self, features, labels):
+        return MSE(self.pred(features), labels)
