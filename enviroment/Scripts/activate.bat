c2b67ed1424632f04ee79a64f01ecb06205e17fb
Julian Müller <julian.mueller18@proton.me>
2024-03-07 17:03:15 +0100

    activate fix

--- enviroment/Scripts/activate.bat
+++ enviroment/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Julian\dev\# spezial projects\Info GK\InfoProjekt\enviroment
+set VIRTUAL_ENV=.\enviroment
 
 if not defined PROMPT set PROMPT=$P$G
 
