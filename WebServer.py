c97f68f5e0fb6de08d99b3cae56c5cfc629ae9bb
Janko Denkert <janko@denkert.eu>
2024-03-07 13:47:13 +0100

    Upload maybe done

--- WebServer.py
+++ WebServer.py
@@ -50,7 +50,7 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(os.path.join(app.root_path, app.config['UPLOAD_FOLDER']), filename))
             return redirect(url_for('download_file', name=filename))
     return '''
     <!doctype html>
